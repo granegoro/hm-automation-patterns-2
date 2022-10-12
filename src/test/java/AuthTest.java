aab0f44f285834e3a587963f5b3fe099973244ea
Егор Старшелюк <granegoro@gmail.com>
2022-10-12 20:49:02 +0300

    Secondary commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,70 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static io.restassured.RestAssured.given;
+
+class AuthTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = DataGenerator.Registration.getRegisteredUser("active");
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new DataGenerator.RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+
+        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
+        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
+        //  пользователя registeredUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
+        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
+        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
+        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
+        //  "Пароль" - пользователя registeredUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
+        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
+        //  "Пароль" - переменную wrongPassword
+    }
+}
---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,87 @@
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    protected static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+                given()
+                .spec(requestSpec)
+                .body(new RegistrationDto(DataGenerator.getRandomLogin(), DataGenerator.getRandomPassword(), "active"))
+                .post("/api/system/users");
+    }
+    // TODO: отправить запрос на указанный в требованиях path, передав в body запроса объект user
+    //  и не забудьте передать подготовленную спецификацию requestSpec.
+    //  Пример реализации метода показан в условии к задаче.
+
+    public static String getRandomLogin() {
+        // TODO: добавить логику для объявления переменной login и задания её значения, для генерации
+        //  случайного логина используйте faker
+        String login = faker.address().firstName();
+        return login;
+    }
+
+    public static String getRandomPassword() {
+        String password = faker.internet().password();
+        return password;
+        // TODO: добавить логику для объявления переменной password и задания её значения, для генерации
+        //  случайного пароля используйте faker
+
+    }
+
+public static class Registration {
+    private Registration() {
+    }
+
+    public static RegistrationDto getUser(String status) {
+
+        new RegistrationDto(
+                getRandomLogin(),
+                getRandomPassword(),
+                status
+        );
+
+        // TODO: создать пользователя user используя методы getRandomLogin(), getRandomPassword() и параметр status
+        /*return user;*/
+    }
+
+    public static RegistrationDto getRegisteredUser(String status) {
+        String registeredUser = Registration.(status);
+        new RegistrationDto(
+                getRandomLogin(),
+                getRandomPassword(),
+                status
+        );
+        // TODO: объявить переменную registeredUser и присвоить ей значение возвращённое getUser(status).
+        // Послать запрос на регистрацию пользователя с помощью вызова sendRequest(registeredUser)
+        /*return registeredUser;*/
+    }
+}
+
+@Value
+public static class RegistrationDto {
+    String login;
+    String password;
+    String status;
+}
+    }
+
+            }
